83195761ce902875b8d2b1cf81350e5744d718f8
4DI <4DI@INF03>
2023-03-04 12:06:41 +0100

    Inizio Programma triangolo 04/03/2023

---
+++ src/Poligono/Triangolo.java
@@ -0,0 +1,24 @@
+package Poligono;
+
+public class Triangolo {
+private float base;
+private float altezza;
+
+public float getBase() {
+	return base;
+}
+public void setBase(float base) {
+	this.base = base;
+}
+public float getAltezza() {
+	return altezza;
+}
+public void setAltezza(float altezza) {
+	this.altezza = altezza;
+}
+
+public String toString() {
+	return "Triangolo [base=" + base + ", altezza=" + altezza + "]";
+}
+
+}
